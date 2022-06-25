7e91fc36aaa56827358fab66f1fe638c9bf645c9
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2022-06-25 19:10:57 +0200

    update project config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { TasksModule } from './tasks.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TasksModule);
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
